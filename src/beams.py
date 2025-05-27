51c9ba80797ad4fa41bfb3e05bb088c0520b7a27
Anas-ja <anasjammoul1999@gmail.com>
2025-05-27 19:13:16 +0300

    add hatch to object

--- src/beams.py
+++ src/beams.py
@@ -3,9 +3,6 @@ import array
 import win32com.client
 import pythoncom
 
-def variants(object):
-    return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_DISPATCH, (object))
-
 def draw_beam_outline(acad, beam_data, layer_name, origin=APoint(0, 0)):
     for beam_name in beam_data:
         for span in beam_data[beam_name]:
@@ -14,7 +11,7 @@ def draw_beam_outline(acad, beam_data, layer_name, origin=APoint(0, 0)):
             dims = beam_data[beam_name][span]["dimensions"]
             width = dims["width_x"]
             height = dims["height_y"]
-            
+
             points = [
                 origin.x, origin.y,
                 origin.x + width, origin.y,
@@ -22,29 +19,51 @@ def draw_beam_outline(acad, beam_data, layer_name, origin=APoint(0, 0)):
                 origin.x, origin.y + height,
                 origin.x, origin.y
             ]
-            
+
             pline = acad.model.AddLightWeightPolyline(array.array("d", points))
             pline.Closed = True
             pline.Layer = layer_name
+            origin.x += width + 10  # shift next beam outline to the right
 
-def draw_rebar(acad, beam_data, layer_name):
+def draw_rebar(acad, beam_data, layer_name, start_origin=APoint(0, 0)):
+    current_x = start_origin.x
+    current_y = start_origin.y
+
+    acad_app = win32com.client.Dispatch("AutoCAD.Application")
+    doc = acad_app.ActiveDocument
+    modelspace = doc.ModelSpace
+
     for beam_name in beam_data:
         for span in beam_data[beam_name]:
             if span == "spans":
                 continue
-            rebar: str = beam_data[beam_name][span]["rebars"]["bottom"]
-            rebar_number = int(rebar.split("T")[0]) # FIXME: no hard coded things use regex
-            rebar_dia = int(rebar.split("T")[1])/10 # FIXME: no hard coded things use regex
-            
-            radius = rebar_dia/2
-            circle = acad.model.AddCircle(APoint(0, 0), radius)
-            circle.Layer = layer_name
-            
-            # hatch = acad.model.AddHatch(0, "SOLID", False)
-            
-            # outer = variants([circle])            
-            # hatch.AppendOuterLoop(outer)
-            # hatch.Evaluate()
-            # hatch.Layer = layer_name
-            
-            
+
+            dims = beam_data[beam_name][span]["dimensions"]
+            width = dims["width_x"]
+            height = dims["height_y"]
+
+            rebar = beam_data[beam_name][span]["rebars"]["bottom"]
+            rebar_number = int(rebar.split("T")[0])
+            rebar_dia = int(rebar.split("T")[1]) / 10
+            radius = rebar_dia / 2
+
+            origin_x = current_x
+            origin_y = current_y
+            spacing = (width - 2 * rebar_dia) / (rebar_number - 1) if rebar_number > 1 else 0
+
+            for i in range(rebar_number):
+                x = origin_x + rebar_dia + i * spacing
+                y = origin_y + rebar_dia
+
+                center = win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, (x, y, 0))
+                circle = modelspace.AddCircle(center, radius)
+                circle.Layer = layer_name
+
+                # Add solid hatch
+                hatch = modelspace.AddHatch(0, "SOLID", True)
+                loop_array = win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_DISPATCH, [circle])
+                hatch.AppendOuterLoop(loop_array)
+                hatch.Evaluate()
+                hatch.Layer = layer_name
+
+            current_x += width + 10  # Move to next beam
--- src/main.py
+++ src/main.py
@@ -1,29 +1,20 @@
-########################
-## File: src/main.py ##
-#######################
-
 from pyautocad import Autocad
-from files_handler import layers_config, beams_data, columns_data
+from files_handler import columns_data, layers_config, beams_data
 from layer_manager import setup_layers
 from beams import draw_beam_outline, draw_rebar
 
 def main():
-    """The main runner function
-    """
+    """The main runner function"""
     acad = Autocad(create_if_not_exists=True)
-    
-    # Layer Manager
+
+    # Setup layers
     setup_layers(acad, layers_config)
-    
-    # Beams Manager
-    draw_beam_outline(acad, beams_data,layer_name="beam_outline")
+
+    # Draw columns and beams
+    draw_beam_outline(acad, beams_data, layer_name="beam_outline")
+
+    # Draw rebar
     draw_rebar(acad, beams_data, layer_name="beam_bottom_rebar")
-    
-    
-    
-    
-    
-    
+
 if __name__ == "__main__":
     main()
-   
