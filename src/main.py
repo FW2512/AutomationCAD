a56158a02ca8f26c25484a74fa425e419a357416
Firas Jar <firasyarmakani@gmail.com>
2025-05-29 19:18:39 +0300

    Update main.py file
    Commented out the beam draw function sice it's not implemented right yet and will cause errors. \n Delted unnecessary imports

--- src/main.py
+++ src/main.py
@@ -1,8 +1,3 @@
-########################
-## File: src/main.py ##
-#######################
-
-from pyautocad import Autocad
 from files_handler import layers_config, beams_data, columns_data
 from layer_manager import setup_layers
 from beams import draw_beam_outline
@@ -10,13 +5,12 @@ from beams import draw_beam_outline
 def main():
     """The main runner function
     """
-    acad = Autocad(create_if_not_exists=True)
     
     # Layer Manager
     setup_layers(layers_config)
     
     # Beams Manager
-    draw_beam_outline(beams_data,layer_name="0")    
+    # draw_beam_outline(beams_data,layer_name="0")
     
     
     
